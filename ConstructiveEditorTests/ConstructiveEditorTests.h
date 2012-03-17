518225595788c68b62bdada87e1b2c049820ee98
bond <admin@ebondarev-imac.local>
2012-03-17 15:10:08 +0200

    Initial Commit

---
+++ ConstructiveEditorTests/ConstructiveEditorTests.h
@@ -0,0 +1,13 @@
+//
+//  ConstructiveEditorTests.h
+//  ConstructiveEditorTests
+//
+//  Created by bond on 3/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ConstructiveEditorTests : SenTestCase
+
+@end
---
+++ ConstructiveEditorTests/ConstructiveEditorTests.m
@@ -0,0 +1,32 @@
+//
+//  ConstructiveEditorTests.m
+//  ConstructiveEditorTests
+//
+//  Created by bond on 3/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ConstructiveEditorTests.h"
+
+@implementation ConstructiveEditorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ConstructiveEditorTests");
+}
+
+@end
