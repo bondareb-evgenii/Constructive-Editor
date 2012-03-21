42795aa58cc04f64ab35becfeedbbc7910321344
Evgenii Bondarev <putnik@ukr.net>
2012-03-21 09:30:33 +0200

    Updated the new file name usages

--- Source/iOS/ConstructiveEditor/DataModels/NSValueToDataTransformer.m
+++ Source/iOS/ConstructiveEditor/DataModels/NSValueToDataTransformer.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "CGSizeToDataTransformer.h"
+#import "NSValueToDataTransformer.h"
 
 @implementation CGSizeToDataTransformer
 
