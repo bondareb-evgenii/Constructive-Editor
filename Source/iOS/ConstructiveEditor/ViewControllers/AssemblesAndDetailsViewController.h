5c7813cb6dd505ed0415a267c9114d35c6d59ca9
Evgenii Bondarev <putnik@ukr.net>
2012-04-11 15:56:34 +0300

    Added view controller sources for other levels of hierarchy (copy of RootAssembliesViewController class yet)

---
+++ Source/iOS/ConstructiveEditor/ViewControllers/AssemblesAndDetailsViewController.h
@@ -0,0 +1,27 @@
+//
+//  AssemblesAndDetailsViewController.h
+//  ConstructiveEditor
+//
+//  Created by Evgenii Bondarev on 3/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssemblesAndDetailsViewController : UIViewController
+  {
+    NSPersistentStoreCoordinator*     _persistentStoreCoordinator;
+    NSManagedObjectModel*             _managedObjectModel;
+    NSManagedObjectContext*           _managedObjectContext;
+    
+    NSIndexPath*                      _selectedIndexPath;
+    NSMutableArray*                   _rootAssembliesArray;
+    NSUInteger                        _addItemIndex;
+    __weak IBOutlet UITableView*      _rootAssembliesTable;
+    __weak IBOutlet UIBarButtonItem*  _editOrDoneButton;
+  }
+
+  - (NSString *)applicationDocumentsDirectory;
+  - (void)saveContext;
+
+@end
