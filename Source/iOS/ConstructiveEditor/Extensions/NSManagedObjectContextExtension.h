41e88edfad3b6bc60d6e7a006ff63e74f4798c64
Evgenii Bondarev <putnik@ukr.net>
2012-04-28 08:01:00 +0300

    Added an extension for the managed object context to save the context (currently errors are just logged to console)

---
+++ Source/iOS/ConstructiveEditor/Extensions/NSManagedObjectContextExtension.h
@@ -0,0 +1,13 @@
+//
+//  CGSizeToDataTransformer.h
+//  Constructive
+//
+//  Created by Evgenii Bondarev on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CGSizeToDataTransformer : NSValueTransformer
+
+@end
