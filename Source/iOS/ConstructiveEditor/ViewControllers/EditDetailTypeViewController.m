a2c0c22732b4859a2c2efadf445c389ebdfcc298
Evgenii Bondarev <putnik@ukr.net>
2013-02-25 18:36:35 +0200

    Added a lift arm ruler which can be zoomed, rotated and moved (it will then give us information about the detail relative zoom factor on a picture)

--- Source/iOS/ConstructiveEditor/ViewControllers/EditDetailTypeViewController.m
+++ Source/iOS/ConstructiveEditor/ViewControllers/EditDetailTypeViewController.m
@@ -29,13 +29,18 @@
   __weak IBOutlet UITextField*        _detailLabelTextField;
   __weak IBOutlet UIStepper*          _detailLengthStepper;
   __weak IBOutlet UILabel*            _detailLengthLabel;
-  __weak IBOutlet UIPickerView*       _detailClassPicker;
+  
   __weak IBOutlet UIView*             _pictureImageViewContainer;
-  __weak IBOutlet UIImageView*        _pictureImageView;
-  __weak IBOutlet UIImageView*        _pinPlatePictureImageView;
-  __weak IBOutlet NSLayoutConstraint* _constraintVerticalSpaceFromPickerToViewContainer;
   __weak IBOutlet NSLayoutConstraint* _constraintViewContainerLeadingSpaceToSuperview;
+  
+  __weak IBOutlet UIPickerView*       _detailClassPicker;
+  __weak IBOutlet NSLayoutConstraint* _constraintVerticalSpaceFromPickerToViewContainer;
   __weak IBOutlet NSLayoutConstraint* _constraintPickerTrainlingSpaceToSuperview;
+  
+  __weak IBOutlet UIImageView*        _pictureImageView;
+  
+  UIImageView*                        _rulerImageView;
+  
   NSArray*                            _constraintsForPortraitOrientation;
   NSArray*                            _constraintsVerticalForLandscapeOrientation;
   NSArray*                            _constraintsHorizontalForLandscapeOrientation;
@@ -123,6 +128,65 @@
     [self updateConstraintsAccordingToInterfaceOrientation:self.interfaceOrientation];
   }
 
+- (void)viewWillAppear:(BOOL)animated
+  {
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //Layout first!
+  [self.view layoutIfNeeded];
+  
+  NSUInteger liftarmLengthInPins = 15;//maximum length of real liftarms
+  UIImage* liftarmImage = [self liftarmImageOfLength:liftarmLengthInPins];
+  
+  _rulerImageView = [[UIImageView alloc] initWithImage:liftarmImage];
+  _rulerImageView.autoresizingMask = UIViewAutoresizingNone;
+  _rulerImageView.translatesAutoresizingMaskIntoConstraints = YES;//make autolayout to simulate the old behavior for this view
+  _rulerImageView.userInteractionEnabled = NO;
+  
+  CGSize superviewSize = _pictureImageViewContainer.bounds.size;
+  CGSize imageSize = liftarmImage.size;
+  float xScale = superviewSize.width/imageSize.width;
+  float yScale = superviewSize.height/imageSize.height;
+  float minScale = xScale;
+  if (minScale > yScale)
+    minScale = yScale;
+    
+  _rulerImageView.center = CGPointMake(superviewSize.width/2, superviewSize.height/2);
+  _rulerImageView.transform = CGAffineTransformScale(_rulerImageView.transform, minScale, minScale);
+  _rulerImageView.alpha = 0.7;
+  
+  [_pictureImageViewContainer addSubview:_rulerImageView];
+  }
+
+- (UIImage*)liftarmImageOfLength:(NSUInteger)length
+  {
+  const NSUInteger countOfCentralSections = length<3 ? 1 :length - 2;
+  
+  const NSUInteger deltaYCentralFromBottomLeftImage = 58;
+  const NSUInteger deltaYCentralFromOtherCentralImage = 58;
+  const NSUInteger deltaYTopRightFromCentralImage = 37;
+  
+  UIImage* bottomLeftEndImage = [UIImage imageNamed:@"liftarm_bottom_left"];
+  UIImage* centerImage = [UIImage imageNamed:@"liftarm_center"];
+  UIImage* topRightEndImage = [UIImage imageNamed:@"liftarm_top_right"];
+  
+  CGFloat resultingWidth = bottomLeftEndImage.size.width + centerImage.size.width*countOfCentralSections + topRightEndImage.size.width;
+  CGFloat resultingHeight = bottomLeftEndImage.size.height + deltaYCentralFromBottomLeftImage + deltaYCentralFromOtherCentralImage*(countOfCentralSections - 1) + deltaYTopRightFromCentralImage;
+
+  // build the actual glued image
+
+  UIGraphicsBeginImageContextWithOptions(CGSizeMake(resultingWidth, resultingHeight), NO, 1);
+  [bottomLeftEndImage drawAtPoint:CGPointMake(0, resultingHeight - bottomLeftEndImage.size.height)];
+  
+  [centerImage drawAtPoint:CGPointMake(bottomLeftEndImage.size.width, resultingHeight - bottomLeftEndImage.size.height - deltaYCentralFromBottomLeftImage)];
+  for (NSUInteger i = 1; i < countOfCentralSections; ++i)
+    [centerImage drawAtPoint:CGPointMake(bottomLeftEndImage.size.width + centerImage.size.width*i, resultingHeight - bottomLeftEndImage.size.height - deltaYCentralFromBottomLeftImage - deltaYCentralFromOtherCentralImage*i)];
+  [topRightEndImage drawAtPoint:CGPointMake(bottomLeftEndImage.size.width + centerImage.size.width*countOfCentralSections, resultingHeight - bottomLeftEndImage.size.height - deltaYCentralFromBottomLeftImage - deltaYCentralFromOtherCentralImage*(countOfCentralSections - 1) - deltaYTopRightFromCentralImage)];
+  
+  UIImage* resultingImage = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return resultingImage;
+  }
+
 - (void)viewWillDisappear:(BOOL)animated
   {
   if (!_detailLabelTextField.hidden)
@@ -176,7 +240,77 @@
   {
   [self selectPicture];
   }
+
+- (IBAction)moveRuler:(UIPanGestureRecognizer *)gestureRecognizer
+  {
+  [self adjustAnchorPointForGestureRecognizer:gestureRecognizer];
   
+  if (gestureRecognizer.state == UIGestureRecognizerStateBegan || gestureRecognizer.state == UIGestureRecognizerStateChanged)
+    {
+    UIView* view = gestureRecognizer.view;
+    CGPoint translation = [gestureRecognizer translationInView:view];
+    
+    _rulerImageView.center = CGPointMake(_rulerImageView.center.x + translation.x, _rulerImageView.center.y + translation.y);
+    [gestureRecognizer setTranslation:CGPointZero inView:view];
+    }
+  }
+
+- (IBAction)zoomRuler:(UIPinchGestureRecognizer *)gestureRecognizer
+  {
+  [self adjustAnchorPointForGestureRecognizer:gestureRecognizer];
+    
+  if (gestureRecognizer.state == UIGestureRecognizerStateBegan || gestureRecognizer.state == UIGestureRecognizerStateChanged)
+    {
+    _rulerImageView.transform = CGAffineTransformScale(_rulerImageView.transform, gestureRecognizer.scale, gestureRecognizer.scale);
+    gestureRecognizer.scale = 1;
+    }
+  }
+
+- (IBAction)rotateRuler:(UIRotationGestureRecognizer *)gestureRecognizer
+  {
+  [self adjustAnchorPointForGestureRecognizer:gestureRecognizer];
+    
+  if (gestureRecognizer.state == UIGestureRecognizerStateBegan || gestureRecognizer.state == UIGestureRecognizerStateChanged)
+    {
+    _rulerImageView.transform = CGAffineTransformRotate(_rulerImageView.transform, gestureRecognizer.rotation);
+    gestureRecognizer.rotation = 0;
+    }
+  }
+
+// scale and rotation transforms are applied relative to the layer's anchor point
+// this method moves a gesture recognizer's view's anchor point between the user's fingers
+- (void)adjustAnchorPointForGestureRecognizer:(UIGestureRecognizer *)gestureRecognizer
+  {
+  if (gestureRecognizer.state == UIGestureRecognizerStateBegan)
+    {
+    UIView* view = gestureRecognizer.view;
+    CGPoint locationInView = [gestureRecognizer locationInView:_rulerImageView];
+    CGPoint locationInSuperview = [gestureRecognizer locationInView:view];
+    
+    _rulerImageView.layer.anchorPoint = CGPointMake(locationInView.x / _rulerImageView.bounds.size.width, locationInView.y / _rulerImageView.bounds.size.height);
+
+    _rulerImageView.center = locationInSuperview;
+    }
+  }
+
+// ensure that the pinch, pan and rotate gesture recognizers on a particular view can all recognize simultaneously
+// prevent other gesture recognizers from recognizing simultaneously
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+  {
+  if (gestureRecognizer.view != _pictureImageViewContainer || otherGestureRecognizer.view != _pictureImageViewContainer)
+    return NO;
+  
+  // if the gesture recognizers are on different views, don't allow simultaneous recognition
+  if (gestureRecognizer.view != otherGestureRecognizer.view)
+    return NO;
+  
+  // if either of the gesture recognizers is the long press, don't allow simultaneous recognition
+  if ([gestureRecognizer isKindOfClass:[UILongPressGestureRecognizer class]] || [otherGestureRecognizer isKindOfClass:[UILongPressGestureRecognizer class]] || [gestureRecognizer isKindOfClass:[UITapGestureRecognizer class]] || [otherGestureRecognizer isKindOfClass:[UITapGestureRecognizer class]])
+    return NO;
+  
+  return YES;
+  }
+
 - (void)updateLengthControls
   {
   int length = _detailType.length.intValue;
